d13baa339c00993c4c67e20410c1361b08e0d480
vithar1 <an.roman.b@gmail.com>
2025-12-12 15:17:38 +0300

    update

--- init.lua
+++ init.lua
@@ -20,20 +20,20 @@ vim.api.nvim_set_keymap("t", "<C-k>", [[<C-\><C-n><C-w>k]], { noremap = true })
 vim.api.nvim_set_keymap("t", "<C-l>", [[<C-\><C-n><C-w>l]], { noremap = true })
 
 -- комментирвоание
-vim.keymap.set("n", "<C-_>", "gcc", { remap = true, desc = "Toggle comment line" })    -- Normal mode
-vim.keymap.set("v", "<C-_>", "gc",  { remap = true, desc = "Toggle comment selection" }) -- Visual mode
+vim.keymap.set("n", "<C-_>", "gcc", { remap = true, desc = "Toggle comment line" })     -- Normal mode
+vim.keymap.set("v", "<C-_>", "gc", { remap = true, desc = "Toggle comment selection" }) -- Visual mode
 
 vim.opt.spell = true
 vim.opt.spelllang = "en,ru"
 
 -- autosave
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-  pattern = "*",
-  callback = function()
-    if vim.bo.modified and vim.bo.modifiable and vim.bo.buftype == "" then
-      vim.cmd("silent! write")
-    end
-  end,
+    pattern = "*",
+    callback = function()
+        if vim.bo.modified and vim.bo.modifiable and vim.bo.buftype == "" then
+            vim.cmd("silent! write")
+        end
+    end,
 })
 
 -- нужно для работы gopls языкового сервера
@@ -41,19 +41,19 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
 local home = vim.fn.expand("~")
 local gobin = home .. "/go/bin"
 if not string.find(vim.env.PATH or "", gobin, 1, true) then
-  vim.env.PATH = (vim.env.PATH and (vim.env.PATH .. ":") or "") .. gobin
+    vim.env.PATH = (vim.env.PATH and (vim.env.PATH .. ":") or "") .. gobin
 end
 
 -- нужно для работы менеджера плагинов
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -61,115 +61,158 @@ vim.opt.rtp:prepend(lazypath)
 vim.keymap.set("n", "<leader><leader>", "<cmd>Telescope buffers<CR>")
 
 require("lazy").setup({
-  	{
-  	  "williamboman/mason.nvim",
-  	  build = ":MasonUpdate",
-  	  config = function()
-  	    require("mason").setup()
-  	  end
-  	},
-  	-- 🔧 LSP + автоподключение
-  	{
-  	  "neovim/nvim-lspconfig",
-  	  config = function()
-  	    require("lspconfig").lua_ls.setup({})
-  	    require("lspconfig").pyright.setup({})
-        -- настройка для гошки. Например чтобы переходить к определению функции     
-        require('lspconfig').gopls.setup {
-            on_attach = function(client, bufnr)
-              local opts = { buffer = bufnr, noremap = true, silent = true }
-              vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-              vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-              vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-              vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-              vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-              vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts) -- просмотр варнигов
-              vim.keymap.set('n', '<leader>f', function()
-                  vim.lsp.buf.format({ async = true })
-              end, { desc = 'Format code' })
-            end,
-            settings = {
-              gopls = {
-                gofumpt = true,
-                staticcheck = true,
-                usePlaceholders = true,
-                analyses = {
-                  unusedparams = true,
-                },
-              },
-            },
-        }
-  	  end
-  	},
-	-- подсветка синтаксиса
-  	{
-  	  "nvim-treesitter/nvim-treesitter",
-  	  build = ":TSUpdate",
-  	  config = function()
-  	    require("nvim-treesitter.configs").setup({
-  	      ensure_installed = { "lua", "python", "bash", "json", "html", "css", "go" },
-  	      highlight = { enable = true },
-  	    })
-  	  end
-  	},
-	-- дерево проекта
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons", -- для иконок файлов
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
+        config = function()
+            require("mason").setup()
+        end
     },
-    config = function()
-      require("nvim-tree").setup({
-        view = {
-          width = 30,
-          side = "left",
-        },
-        actions = {
-          open_file = {
-            quit_on_open = false,
-          },
-        },
-        renderer = {
-          icons = {
-            show = {
-              file = true,
-              folder = true,
-              folder_arrow = true,
-              git = true,
-            },
-          },
-        },
-        filters = {
-          dotfiles = false,
-        },
-        update_focused_file = {
-          enable = true,
-        },
+    -- 🔧 LSP + автоподключение
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local on_attach = function(client, bufnr)
+                local opts = { buffer = bufnr, noremap = true, silent = true }
+                -- Перейти к определению
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                -- перейти к использованию
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                -- Документация
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                -- Рефакторинг
+                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                -- Диагностика
+                vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+                -- Форматирование
+                vim.keymap.set('n', '<leader>f', function()
+                    vim.lsp.buf.format({ async = true })
+                end, { desc = 'Format code', buffer = bufnr })
+            end
 
-        -- Переопределяем маппинги для открытия файлов
-        on_attach = function(bufnr)
-            local api = require("nvim-tree.api")
-            api.config.mappings.default_on_attach(bufnr)
+            -- gopls — запуск только для файлов Go
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "go",
+                callback = function()
+                    vim.lsp.start({
+                        name = "gopls",
+                        cmd = { "gopls" },
+                        root_dir = vim.lsp.config.util.root_pattern("go.mod", ".git"),
+                        settings = {
+                            gopls = {
+                                gofumpt = true,
+                                staticcheck = true,
+                                usePlaceholders = true,
+                                analyses = {
+                                    unusedparams = true,
+                                },
+                            },
+                        },
+                        on_attach = on_attach,
+                    })
+                end,
+            })
+            -- lua-language-server (lua_ls)
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "lua",
+                callback = function()
+                    local util = require("lspconfig.util")
+                    vim.lsp.start({
+                        name = "lua_ls",
+                        cmd = { "lua-language-server" },
+                        root_dir = util.root_pattern(".git", ".config", "lua") or vim.fn.getcwd(),
+                        settings = {
+                            Lua = {
+                                workspace = {
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                    checkThirdParty = false,
+                                },
+                                diagnostics = {
+                                    globals = { "vim" },
+                                },
+                                completion = {
+                                    callSnippet = "Replace",
+                                },
+                            },
+                        },
+                        on_attach = on_attach,
+                    })
+                end,
+            })
+        end
+    },
+
+
+    -- подсветка синтаксиса
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "python", "bash", "json", "html", "css", "go" },
+                highlight = { enable = true },
+            })
+        end
+    },
+    -- дерево проекта
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons", -- для иконок файлов
+        },
+        config = function()
+            require("nvim-tree").setup({
+                view = {
+                    width = 30,
+                    side = "left",
+                },
+                actions = {
+                    open_file = {
+                        quit_on_open = false,
+                    },
+                },
+                renderer = {
+                    icons = {
+                        show = {
+                            file = true,
+                            folder = true,
+                            folder_arrow = true,
+                            git = true,
+                        },
+                    },
+                },
+                filters = {
+                    dotfiles = false,
+                },
+                update_focused_file = {
+                    enable = true,
+                },
+
+                -- Переопределяем маппинги для открытия файлов
+                on_attach = function(bufnr)
+                    local api = require("nvim-tree.api")
+                    api.config.mappings.default_on_attach(bufnr)
+                end,
+            })
+
+            -- Маппинг для открытия/закрытия дерева по <leader>e
+            vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
         end,
-      })
-
-      -- Маппинг для открытия/закрытия дерева по <leader>e
-      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-    end,
-  },
+    },
     -- комментирование кода
-  {
-    "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup()
-    end,
-  },
-  -- переключение между последними файлами
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        require("telescope").setup()
-    end,
-  }
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end,
+    },
+    -- переключение между последними файлами
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("telescope").setup()
+        end,
+    }
 })
